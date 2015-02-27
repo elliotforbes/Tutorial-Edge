5b6474a4917ce09c975a1e38081426fab31909c6
emforce <emforce@live.com>
2015-02-27 14:18:11 +0000

    stuff

---
+++ src/Paddle.java
@@ -0,0 +1,30 @@
+
+public class Paddle {
+	
+	public float xPos, yPos;
+	
+	public Paddle(){
+		
+	}
+	
+	public void setXPos(float x){
+		this.xPos = x;
+	}
+	
+	public void setYPos(float y){
+		this.yPos = y;
+	}
+	
+	public float getX(){
+		return xPos;
+	}
+	
+	public float getY(){
+		return yPos;
+	}
+	
+	public void draw(){
+		
+	}
+
+}
